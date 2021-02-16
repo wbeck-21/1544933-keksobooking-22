c0cff34a6278bd52ce5f9f78bc465d0d0291c935
Анастасия <goodmorning.25@mail.ru>
2021-02-16 19:11:30 +0300

    Исправление ошибок на тему функции

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const getRandomInteger = (begin, end, quantitySigns) => {
   if (begin >= 0 && end >= 0 && begin < end) {
-    return parseFloat((Math.random() * (begin - end + 1) + end).toFixed(quantitySigns));
+    return parseFloat((begin + (Math.random() * (begin - end))).toFixed(quantitySigns));
   }
   if (begin === end) {
     return begin;
@@ -8,4 +8,4 @@ const getRandomInteger = (begin, end, quantitySigns) => {
   console.log('Упс, что-то пошло не так...');
 }
 
-console.log(getRandomInteger(4.2, 4.2, 4));
+console.log(getRandomInteger(4.2, 4.6, 4));
