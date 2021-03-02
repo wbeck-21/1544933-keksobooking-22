4f17c7f7d605c9e5abe35828abf13059d0d9839b
Анастасия <goodmorning.25@mail.ru>
2021-03-02 09:23:45 +0300

    Деление кода на модули

---
+++ js/util.js
@@ -0,0 +1,11 @@
+const getRandomInteger = (begin, end, quantitySigns) => {
+  if (begin >= 0 && end >= 0 && begin < end) {
+    return parseFloat((begin + (Math.random() * ((end - begin)) + 1)).toFixed(quantitySigns));
+  }
+  if (begin === end) {
+    return begin;
+  }
+  return null;
+}
+
+export {getRandomInteger};
