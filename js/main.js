2da609ec58f45011379f7afec0efffecff6c1223
Анастасия <goodmorning.25@mail.ru>
2021-03-02 16:36:19 +0300

    Исправления по модулям

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
-import {generateData} from './data.js';
+import {generateAvatar} from './util.js';
+import {generateTitle} from './util.js';
+import {generateX} from './util.js';
+import {generateY} from './util.js';
+import {generateDiscription} from './util.js';
+import {generateAddress} from './util.js';
+import {generatePrice} from './util.js';
+import {generateType} from './util.js';
+import {generateRooms} from './util.js';
+import {generateGuests} from './util.js';
+import {generateCheckIn} from './util.js';
+import {generateCheckOut} from './util.js';
+import {generateFeatures} from './util.js';
+import {generatePhotos} from './util.js';
+
+import {allTitles} from './data.js';
+import {allDescriptions} from './data.js';
+import {allTypes} from './data.js';
+import {allCheckIn} from './data.js';
+import {allCheckOut} from './data.js';
+import {possibleFeatures} from './data.js';
+import {possiblePhotos} from './data.js';
+
+const generateObj = () => {
+  let obj = {};
+
+  obj.author = {};
+  obj.author.avatar = generateAvatar();
+
+  obj.offer = {};
+  obj.offer.title = generateTitle(allTitles);
+  obj.offer.descriptions = generateDiscription(allDescriptions);
+  obj.offer.address = generateAddress();
+  obj.offer.price = generatePrice();
+  obj.offer.type = generateType(allTypes);
+  obj.offer.rooms = generateRooms();
+  obj.offer.guests = generateGuests();
+  obj.offer.checkin = generateCheckIn(allCheckIn);
+  obj.offer.checkout = generateCheckOut(allCheckOut);
+  obj.offer.features = generateFeatures(possibleFeatures);
+  obj.offer.photos = generatePhotos(possiblePhotos);
+
+  obj.location = {};
+  obj.location.x = generateX();
+  obj.location.y = generateY();
+
+  return obj;
+}
+
+
+const generateData = (n) => {
+  let data = [];
+
+  for (let i = 0; i < n; i++) {
+    data[i] = generateObj();
+  }
+
+  return data;
+}
 
 console.log(generateData(10));
+
