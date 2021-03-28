3b1ef0924f405f55e45885d7c26cbe1a03299c3c
Анастасия <goodmorning.25@mail.ru>
2021-03-28 14:37:57 +0300

    Рефакторинг кода

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { generateDataList } from './data.js';
 import { renderData } from './render.js';
-// import { mapCanvas } from './map.js';
+import { renderMap } from './map.js';
 
 
 const advertismentForm = document.querySelector('.ad-form');
@@ -23,46 +23,6 @@ for (let i = 0; i < mapFilterElement.children.length; i++) {
 
 
 const dataList = generateDataList();
+renderMap(dataList, renderData);
 
 
-let map = L.map('map-canvas').setView([35.652832, 139.839478], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-const pinIcon = L.icon({
-  iconUrl: '../img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
-
-dataList.map(dataItem => {
-  L.marker([dataItem.location.x, dataItem.location.y], {icon: pinIcon}).addTo(map)
-    .bindPopup(renderData(dataItem))
-    .openPopup();
-});
-
-const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
-
-
-const mainPinMarker = L.marker(
-  {
-    lng: 139.839478,
-    lat: 35.652832,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
-  },
-)
-.addTo(map);
-
-mainPinMarker.on('moveend', (evt) => {
-  console.log(evt.target.getLatLng());
-});
--- js/map.js
+++ js/map.js
@@ -1,1 +1,45 @@
+const renderMap = (dataList, renderData) => {
+  let map = L.map('map-canvas').setView([35.652832, 139.839478], 13);
 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  const pinIcon = L.icon({
+    iconUrl: '../img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  dataList.map(dataItem => {
+    L.marker([dataItem.location.x, dataItem.location.y], { icon: pinIcon }).addTo(map)
+      .bindPopup(renderData(dataItem))
+      .openPopup();
+  });
+
+  const mainPinIcon = L.icon({
+    iconUrl: '../img/main-pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+
+  const mainPinMarker = L.marker(
+    {
+      lng: 139.839478,
+      lat: 35.652832,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  )
+    .addTo(map);
+
+  mainPinMarker.on('moveend', (evt) => {
+    console.log(evt.target.getLatLng());
+  });
+}
+
+export { renderMap };
